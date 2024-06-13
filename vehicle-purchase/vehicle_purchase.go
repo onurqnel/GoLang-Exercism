deb7b0fe751026d398433fd3b56a5681111ff825
onurqnel <onur.onel2230@gmail.com>
2024-06-13 18:23:05 -0400

    Golang Conditionals

---
+++ vehicle-purchase/vehicle_purchase.go
@@ -0,0 +1,32 @@
+package purchase
+
+// NeedsLicense determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
+func NeedsLicense(kind string) bool {
+	if kind == "car" || kind == "truck" {
+		return true
+	} else {
+		return false
+	}
+}
+
+// ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
+func ChooseVehicle(option1, option2 string) string {
+	if option1 < option2 {
+		return option1 + " is clearly the better choice."
+	} else {
+		return option2 + " is clearly the better choice."
+
+	}
+}
+
+// CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
+func CalculateResellPrice(originalPrice, age float64) float64 {
+	if age < 3 {
+		return float64(originalPrice) * 0.80
+	} else if age >= 3 && age < 10 {
+		return float64(originalPrice) * 0.70
+
+	} else {
+		return float64(originalPrice) * 0.50
+	}
+}
