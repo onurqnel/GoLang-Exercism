ad9ea9b6ca36c27b6235d30415ab6d6f7a1024f2
onurqnel <onur.onel2230@gmail.com>
2024-06-29 17:54:22 -0400

    Structs and Instances

---
+++ need-for-speed/need_for_speed.go
@@ -0,0 +1,48 @@
+package speed
+
+type Car struct {
+	battery      int
+	speed        int
+	batteryDrain int
+	distance     int
+}
+
+// NewCar creates a new remote controlled car with full battery and given specifications.
+func NewCar(speed, batteryDrain int) Car {
+	return Car{
+		speed:        speed,
+		batteryDrain: batteryDrain,
+		distance:     0,
+		battery:      100,
+	}
+
+}
+
+type Track struct {
+	distance int
+}
+
+// NewTrack creates a new track
+func NewTrack(distance int) Track {
+	return Track{
+		distance: distance,
+	}
+}
+
+// Drive drives the car one time. If there is not enough battery to drive one more time,
+// the car will not move.
+func Drive(car Car) Car {
+	if car.batteryDrain > car.battery {
+		return car
+	}
+	car.battery -= car.batteryDrain
+	car.distance += car.speed
+
+	return car
+}
+
+// CanFinish checks if a car is able to finish a certain track.
+func CanFinish(car Car, track Track) bool {
+	totalDrainRequired := (track.distance / car.speed) * car.batteryDrain
+	return car.battery >= totalDrainRequired
+}
